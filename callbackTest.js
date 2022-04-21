e9d658f8f29bed4464e45a7c2fdbffd4e40e825d
toaded-git <70682418+toaded-git@users.noreply.github.com>
2022-04-21 16:04:42 +0900

    Add files via upload

---
+++ callbackTest.js
@@ -0,0 +1,72 @@
+// function run() {
+//     const start = Date.now();
+//     for (let i = 0; i < 1000000000; i++) {}
+//     const end = Date.now();
+//     console.log(end - start + 'ms');
+//   }
+ 
+//   run();
+//   console.log('Done !!!');
+
+// function run() {
+//     setTimeout(() => {
+//       const start = Date.now();
+//       for (let i = 0; i < 1000000000; i++) {}
+//       const end = Date.now();
+//       console.log(end - start + 'ms');
+//     }, 1000);
+// }
+
+// run()
+// console.log('')
+
+
+// function run(callback) {
+//     setTimeout(() => {
+//       const start = Date.now();
+//       for (let i = 0; i < 1000000000; i++) {}
+//       const end = Date.now();
+//       console.log(end - start + 'ms');
+//       callback();
+//     }, 1000);
+//   }
+ 
+//   run(() => {
+//     console.log('Done !!!');
+//   });
+
+
+// function run() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             const start = Date.now();
+//             for (let i = 0; i < 1000000000; i++) {}
+//             const end = Date.now();
+//             console.log(end - start + 'ms');
+//             resolve();
+//         }, 1000);
+//     });
+// }
+ 
+// run().then(() => {console.log('Done !!!');});
+// run().then(() => {return run()}).then(() => {console.log('Done !!!');});
+
+function run() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const start = Date.now();
+            for (let i = 0; i < 1000000000; i++) {}
+            const end = Date.now();
+            console.log(end - start + 'ms');
+            resolve();
+        }, 1000);
+    });
+}
+ 
+async function process() {
+    await run();
+    await run();
+    console.log('Done !!!');
+}
+ 
+process();
